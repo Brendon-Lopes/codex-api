2084862641bd02dbc612b3c7b33b9582769eaf6a
Brendon <brendon.lopes.21@gmail.com>
2023-03-20 02:54:30 -0300

    feat: add connection to prisma on user-auth
    repository layer

--- src/modules/user-auth/repositories/user-auth-repository.ts
+++ src/modules/user-auth/repositories/user-auth-repository.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@nestjs/common'
-import { User } from '../entities/user.entity'
 import { IUserAuthRepository } from '../interfaces/user-auth-repository.interface'
+import { User } from '@prisma/client'
+import { PrismaService } from '../../../database/prisma/prisma.service'
 
 @Injectable()
 export class UserAuthRepository implements IUserAuthRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
   async findUserByEmail(email: string): Promise<User> {
-    const user = new User()
-    user.email = email
-    return user
+    return await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
   }
 }
