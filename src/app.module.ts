abcf134378eab2c00cfc2f7c7ada3cd1bb00279a
Brendon <brendon.lopes.21@gmail.com>
2023-03-20 00:57:04 -0300

    feat: add user-auth resource to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { UserAuthModule } from './modules/user-auth/user-auth.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserAuthModule],
 })
 export class AppModule {}
