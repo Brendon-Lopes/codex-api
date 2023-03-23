8f8971e599a6840d12043c44e751d161c914e939
Brendon <brendon.lopes.21@gmail.com>
2023-03-23 00:41:56 -0300

    feat: add interfaces for register

--- src/modules/user-auth/interfaces/user-auth-repository.interface.ts
+++ src/modules/user-auth/interfaces/user-auth-repository.interface.ts
@@ -1,5 +1,7 @@
-import { User } from '../entities/user.entity'
+import { User } from '@prisma/client'
+import { UserRegisterDto } from '../dto/user-register.dto'
 
 export abstract class IUserAuthRepository {
   abstract findUserByEmail(email: string): Promise<User>
+  abstract createUser(userRegisterDto: UserRegisterDto): Promise<User>
 }
--- src/modules/user-auth/interfaces/user-auth-service.interface.ts
+++ src/modules/user-auth/interfaces/user-auth-service.interface.ts
@@ -1,6 +1,8 @@
 import { UserLoginDto } from '../dto/user-login.dto'
-import { User } from '../entities/user.entity'
+import { User } from '@prisma/client'
+import { UserRegisterDto } from '../dto/user-register.dto'
 
 export abstract class IUserAuthService {
   abstract login(userLoginDto: UserLoginDto): Promise<User>
+  abstract register(userRegisterDto: UserRegisterDto): Promise<User>
 }
