1ca1081e5c730ce2eadf25343d238ce2b0ac4a23
Brendon <brendon.lopes.21@gmail.com>
2023-06-02 00:24:41 -0300

    fix: interfaces with incorrect return types

--- src/modules/user-auth/interfaces/user-auth-service.interface.ts
+++ src/modules/user-auth/interfaces/user-auth-service.interface.ts
@@ -1,11 +1,10 @@
 import { UserLoginDto } from '../dto/user-login.dto'
-import { User } from '@prisma/client'
 import { UserRegisterDto } from 'src/modules/users/dto/user-register.dto'
+import { ILoginResponse } from './login-response.interface'
+import { IRegisterResponse } from './register-response.interface'
 
 export abstract class IUserAuthService {
-  abstract login(userLoginDto: UserLoginDto): Promise<Omit<User, 'password'>>
+  abstract login(userLoginDto: UserLoginDto): Promise<ILoginResponse>
 
-  abstract register(
-    userRegisterDto: UserRegisterDto,
-  ): Promise<Omit<User, 'password'>>
+  abstract register( userRegisterDto: UserRegisterDto): Promise<IRegisterResponse>
 }
