0d27f5fc7c4da709963878c83a078604dff54706
Brendon <brendon.lopes.21@gmail.com>
2023-06-02 00:19:57 -0300

    fix: interfaces with incorrect return types
    also removes unused repositories

--- src/modules/user-auth/user-auth.module.ts
+++ src/modules/user-auth/user-auth.module.ts
@@ -3,9 +3,7 @@ import { Module } from '@nestjs/common'
 import { PrismaService } from 'src/database/prisma/prisma.service'
 import { UsersModule } from '../users/users.module'
 import { UserAuthController } from './controllers/user-auth.controller'
-import { IUserAuthRepository } from './interfaces/user-auth-repository.interface'
 import { IUserAuthService } from './interfaces/user-auth-service.interface'
-import { UserAuthRepository } from './repositories/user-auth-repository'
 import { UserAuthService } from './services/user-auth.service'
 import { JwtModule } from '@nestjs/jwt'
 import { PassportModule } from '@nestjs/passport'
@@ -29,10 +27,6 @@ const JWT_SECRET = process.env.JWT_SECRET
     {
       provide: IUserAuthService,
       useClass: UserAuthService,
-    },
-    {
-      provide: IUserAuthRepository,
-      useClass: UserAuthRepository,
     },
     UserJwtService,
     PrismaService,
